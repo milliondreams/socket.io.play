595dbf5b74a1ed7f99c84c7425428bf6d1b7c7fb
Rohit <rohit@tingendab.com>
2012-05-04 06:32:11 +0530

    Basic socket.io over websocket is working now.

--- app/controllers/SocketIO.scala
+++ app/controllers/SocketIO.scala
@@ -13,6 +13,7 @@ import play.api.Play.current
 
 import akka.util.Timeout
 import akka.pattern.ask
+import com.sun.xml.internal.ws.handler.ClientMessageHandlerTube
 
 object SocketIO extends Controller {
 
@@ -22,13 +23,13 @@ object SocketIO extends Controller {
     Akka.system.actorOf(Props[SocketIOActor])
   }
 
-  /* def init = Action {
+  def init = Action {
     val sessionId = java.util.UUID.randomUUID().toString()
     println(sessionId)
-    Ok(sessionId + ":1:10:websocket")
-  } */
+    Ok(sessionId + ":15:10:websocket")
+  }
 
-  def socketSetup(sessionId: String) = WebSocket.async[JsValue] {
+  def socketSetup(sessionId: String) = WebSocket.async[String] {
     request =>
       (socketIOActor ? Join(sessionId)).asPromise.map {
 
@@ -36,15 +37,19 @@ object SocketIO extends Controller {
 
           println("Connected")
           // Create an Iteratee to consume the feed
-          val iteratee = Iteratee.foreach[JsValue] {
+          val iteratee = Iteratee.foreach[String] {
             event =>
               println("Talking -- " + event)
-              socketIOActor ! Message(sessionId, event)
+              socketIOActor ! ServerMessage(sessionId, event)
           }.mapDone {
             _ =>
               println("Quit!!!")
               socketIOActor ! Quit(sessionId)
           }
+
+          //println("Sending connect response")
+          socketIOActor ! ClientMessage(sessionId, "1::")
+          socketIOActor ! ClientMessage(sessionId, ":5:::{\"name\":\"eventConnect\",\"args\":[{\"message\":\"welcome\"}]}")
 
           (iteratee, enumerator)
 
@@ -53,10 +58,10 @@ object SocketIO extends Controller {
           // Connection error
 
           // A finished Iteratee sending EOF
-          val iteratee = Done[JsValue, Unit]((), Input.EOF)
+          val iteratee = Done[String, Unit]((), Input.EOF)
 
           // Send an error and close the socket
-          val enumerator = Enumerator[JsValue](error).andThen(Enumerator.enumInput(Input.EOF))
+          val enumerator = Enumerator[String](error).andThen(Enumerator.enumInput(Input.EOF))
 
           (iteratee, enumerator)
 
@@ -67,25 +72,24 @@ object SocketIO extends Controller {
 
 class SocketIOActor extends Actor {
 
-  var sessions = Map.empty[String, PushEnumerator[JsValue]]
+  var sessions = Map.empty[String, PushEnumerator[String]]
 
   def receive = {
     case Join(sessionId) => {
       println(sessionId)
-      val channel = Enumerator.imperative[JsValue]()
+      val channel = Enumerator.imperative[String]()
       if (sessions.contains(sessionId)) {
-        sender ! CannotConnect(Json.toJson(Map("error" -> "This username is already used")))
+        sender ! CannotConnect(Json.stringify(Json.toJson(Map("error" -> "Invalid Session ID"))))
       } else {
         sessions = sessions + (sessionId -> channel)
-
         sender ! Connected(channel)
       }
     }
-    case Message(sessionId, event) => {
+    case ServerMessage(sessionId, event) => {
       println(sessionId + "---" + event)
       //DO your message processing here! Like saving the data
       val id = math.round(math.random * 1000)
-      notify(sessionId, Json.toJson(Map("id" -> id)))
+      notify(sessionId, ":3:::" + Json.stringify(Json.toJson(Map("id" -> id))))
 
     }
 
@@ -93,11 +97,16 @@ class SocketIOActor extends Actor {
       sessions = sessions - sessionId
       println(sessionId + "--- QUIT")
     }
+
+    case ClientMessage(sessionId, message) => {
+      println("Sending connect response -- " + message)
+      notify(sessionId, message)
+    }
   }
 
-  def notify(sessionId:String, response:JsValue) {
-    //Sending data back here
-    sessions(sessionId).push(response)
+  def notify(sessionId:String, message:String) {
+    println("Sending message -- " + message)
+    sessions(sessionId).push(message)
   }
 }
 
@@ -105,10 +114,12 @@ case class Join(sessionId: String)
 
 case class Heartbeat(sessionId: String)
 
-case class Message(sessionId:String, message:JsValue)
+case class ServerMessage(sessionId:String, message:String)
+
+case class ClientMessage(sessionId:String, message:String)
 
 case class Quit(sessionId: String)
 
-case class CannotConnect(message: JsValue)
+case class CannotConnect(message: String)
 
-case class Connected(enumerator: Enumerator[JsValue])
+case class Connected(enumerator: Enumerator[String])
