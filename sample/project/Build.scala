9aaa0495a522ae9b505008401780380bd7d22fff
Ben Phelan <bemaph@gmail.com>
2013-04-27 22:25:24 +0200

    Project compiles for Scala 2.10 and Play 2.1.

--- sample/project/Build.scala
+++ sample/project/Build.scala
@@ -1,21 +1,24 @@
 import sbt._
 import Keys._
-import PlayProject._
+import play.Project._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "socket.io.play.sample"
+    val appName         = "socket-io-play-sample"
     val appVersion      = "1.0"
 
     val appDependencies = Seq(
       // Add your project dependencies here,
-      "com.imaginea" %% "socket.io.play" % "0.0.3-SNAPSHOT"
+      "com.imaginea" %% "socket-io-play" % "0.0.3-SNAPSHOT"
     )
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+    val main = play.Project(appName, appVersion, appDependencies).settings(
       // Add your own project settings here      
       // resolvers += "Local Play Repository" at "file://home/rohit/.ivy2/local"
       resolvers += "OSS Repo" at "https://oss.sonatype.org/content/repositories/snapshots"
     )
 
 }
+
+
+
