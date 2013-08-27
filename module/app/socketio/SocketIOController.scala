d4eea768d1985692cd4d61437c5fd764e0bc93ae
Saurabh Rawat <sr.eklavya@gmail.com>
2013-08-27 14:04:01 +0530

    upload in module

--- module/app/socketio/SocketIOController.scala
+++ module/app/socketio/SocketIOController.scala
@@ -1,12 +1,9 @@
 package socketio
 
-import scala.concurrent._
 import scala.concurrent.duration._
 import scala.util.matching.Regex
 
-import play.api._
 import play.api.libs.json._
-import play.api.libs.json.JsValue
 import play.api.libs.iteratee._
 import play.api.libs.concurrent._
 import play.api.libs.concurrent.Execution.Implicits._
@@ -16,14 +13,15 @@ import play.api.Play.current
 import akka.actor._
 import akka.pattern.ask
 import akka.util.Timeout
-import akka.routing._
 
-import socketio.PacketTypes._
-
+import PacketTypes._
 
 trait SocketIOController extends Controller {
 
-  val socketIOActor: ActorRef
+  val xhrMap = collection.mutable.Map.empty[String, ActorRef]
+  val wsMap = collection.mutable.Map.empty[String, ActorRef]
+
+  def processMessage(sessionId: String, packet: Packet): Unit
 
   implicit val timeout = Timeout(10 second)
 
@@ -34,51 +32,73 @@ trait SocketIOController extends Controller {
 
     Option(transport) match {
       case None => initSession
-      case Some("websocket") => websocketSetup(sessionId)
-      case _  => throw new Exception("Unable to match transport")
+      case Some("websocket") => wsHandler(sessionId)
+      case Some("xhr-polling") => xhrHandler(sessionId)
+      case _ => throw new Exception("Unable to match transport")
     }
+  }
+
+  def enqueueMsg(sessionId: String, msg: String) = enqueue(sessionId, MESSAGE, msg)
+
+  def enqueueEvent(sessionId: String, event: String) = enqueue(sessionId, EVENT, event)
+
+  def enqueueJsonMsg(sessionId: String, msg: String) = enqueue(sessionId, PacketTypes.JSON, msg)
+
+  def enqueue(sessionId: String, payload: String, payloadType: String) {
+    if (wsMap contains sessionId)
+      wsMap(sessionId) ! Enqueue(Parser.encodePacket(Packet(packetType = payloadType, endpoint = "", data = payload)))
+    else if (xhrMap contains sessionId)
+      xhrMap(sessionId) ! Enqueue(Parser.encodePacket(Packet(packetType = payloadType, endpoint = "", data = payload)))
+  }
+
+  def broadcastMsg(msg: String) = {
+    xhrMap.keysIterator.foreach(enqueueMsg(_, msg))
+    wsMap.keysIterator.foreach(enqueueMsg(_, msg))
+  }
+
+  def broadcastEvent(event: String) = {
+    xhrMap.keysIterator.foreach(enqueueEvent(_, event))
+    wsMap.keysIterator.foreach(enqueueEvent(_, event))
+  }
+
+  def broadcastJsonMsg(msg: String) = {
+    xhrMap.keysIterator.foreach(enqueueJsonMsg(_, msg))
+    wsMap.keysIterator.foreach(enqueueJsonMsg(_, msg))
   }
 
   def initSession = Action {
-    val sessionId = java.util.UUID.randomUUID().toString()
-    println(sessionId)
-    Ok(sessionId + ":20:15:websocket")
+    val sessionId = java.util.UUID.randomUUID().toString
+    System.err.println("Strating new session: " + sessionId)
+    Ok(sessionId + ":20:15:websocket,xhr-polling")
   }
 
-  def websocketSetup (sessionId: String) = WebSocket.async[String] {
-    request =>
-      (socketIOActor ? Join(sessionId)).map {
-        case ConnectionEstablished(enumerator) =>
-          handleConnectionSetup(sessionId, enumerator)
-
-        case NotifyConnectFailure(error) =>
-          handleConnectionFailure(error)
+  def wsHandler(sessionId: String) = WebSocket.using[String] {
+    implicit request =>
+      if (wsMap contains sessionId) {
+        handleConnectionFailure(Json.stringify(Json.toJson(Map("error" -> "Invalid Session ID"))))
+      } else {
+        println("creating new websocket actor")
+        val channel = Enumerator.imperative[String]()
+        val wsActor = Akka.system.actorOf(Props(new WSActor(channel, processMessage, wsMap)))
+        wsMap += (sessionId -> wsActor)
+        handleConnectionSetup(sessionId, channel)
       }
   }
 
-  def handleConnectionSetup (sessionId: String, enumerator: Enumerator[String]):
+  def handleConnectionSetup(sessionId: String, enumerator: Enumerator[String]):
   (Iteratee[String, Unit], Enumerator[String]) = {
-    println("ConnectionEstablished")
-    // Create an Iteratee to consume the feed
     val iteratee = Iteratee.foreach[String] {
       socketData =>
-
-        println("Got this -- " + socketData)
-
-        socketIOActor ! ProcessRawSocketData(sessionId, socketData)
-
+        wsMap(sessionId) ! ProcessPacket(socketData)
     }.mapDone {
       _ =>
-        println("Quit!!!")
-        socketIOActor ! Quit(sessionId)
+        println("all done quit.")
     }
-
-    socketIOActor ! NotifyConnected(sessionId, "")
-
+    wsMap(sessionId) ! EventOrNoop
     (iteratee, enumerator)
   }
 
-  def handleConnectionFailure (error: String): (Iteratee[String, Unit], Enumerator[String]) = {
+  def handleConnectionFailure(error: String): (Iteratee[String, Unit], Enumerator[String]) = {
     // Connection error
 
     // A finished Iteratee sending EOF
@@ -89,222 +109,144 @@ trait SocketIOController extends Controller {
 
     (iteratee, enumerator)
   }
+
+  def xhrHandler(sessionId: String) = Action {
+    implicit request =>
+      if (xhrMap contains sessionId) {
+        val body: AnyContent = request.body
+        body.asText match {
+          case None => Async {
+            val res = xhrMap(sessionId) ? EventOrNoop
+            res.map {
+              x: Any =>
+                Ok(x.toString)
+            }
+          }
+
+          case Some(x) => Async {
+            val res = ask(xhrMap(sessionId), ProcessPacket(x))
+            res.map {
+              x: Any =>
+                Ok(x.toString)
+            }
+          }
+        }
+      } else {
+        val xhrActor = Akka.system.actorOf(Props(new XHRActor(processMessage, xhrMap)))
+        xhrMap += (sessionId -> xhrActor)
+        Async {
+          val res = xhrActor ? EventOrNoop
+          res.map {
+            x: Any =>
+              Ok(x.toString)
+          }
+        }
+      }
+  }
 }
 
-trait SocketIOActor extends Actor {
+class XHRActor(val processMessage: (String, Packet) => Unit, val xhrMap: collection.mutable.Map[String, ActorRef]) extends Actor {
+  var eventQueue = List.empty[String]
 
-  var sessions = Map.empty[String, SocketIOSession]
-  val timeout = 10 second
+  def enqueue: Receive = {
+    case Enqueue(x) => eventQueue = eventQueue :+ x
+  }
 
-  val connectHandler = context.actorOf(Props(new Actor {
-    def receive = {
-      case NotifyConnected(sessionId, namespace) => {
-        packetSender ! SendPacket(sessionId, Packet(packetType = CONNECT, endpoint = namespace))
-        processMessage("connected", (sessionId, namespace, ""))
-      }
-    }
-  }).withRouter(FromConfig()), "connectHandler")
-  
-  val heartbeatHandler = context.actorOf(Props(new Actor {
-    def receive = {
-      case Heartbeat(sessionId) => {
-        packetSender ! SendPacket(sessionId, Packet(packetType = HEARTBEAT))        }
-      }
-  }).withRouter(FromConfig()), "heartbeatHandler")
-
-  val messageHandler = context.actorOf(Props(new Actor {
-    def receive = {
-      case ReceiveMessage(sessionId, namespace, msg) => {
-        println("RECEIVED---" + sessionId + "---" + msg)          
-        processMessage("message", (sessionId, namespace, msg))
-      }
-    }
-  }).withRouter(FromConfig()), "messageHandler")
-
-  val jsonHandler = context.actorOf(Props(new Actor {
-    def receive = {
-      case ReceiveJsonMessage(sessionId, namespace, json) => {
-        println(sessionId + "---" + json)
-        if (processMessage.isDefinedAt(("message", (sessionId, namespace, json)))) {
-          processMessage("message", (sessionId, namespace, json))
-        } else {
-          processMessage("message", (sessionId, namespace, Json.stringify(json)))
+  def sendEventOrNoop(s: ActorRef) {
+    eventQueue match {
+      case x :: xs => eventQueue = xs; s ! x
+      case Nil => context.system.scheduler.scheduleOnce(9.seconds) {
+        eventQueue match {
+          case x :: xs => eventQueue = xs; s ! x
+          case Nil => s ! "8::"
         }
       }
-
     }
-  }).withRouter(FromConfig()), "jsonHandler")
+  }
 
-  val eventHandler = context.actorOf(Props(new Actor {
-    def receive = {
-      case ReceiveEvent(sessionId, namespace, eventName, eventData) => {
-        println(sessionId + "---" + eventName + " -- " + eventData)
-        processMessage(eventName, (sessionId, namespace, eventData))
-      }
-    }
-  }).withRouter(FromConfig()), "eventHandler")
+  def receive = enqueue orElse beforeConnected
 
-  val packetSender: ActorRef = context.actorOf(Props(new Actor {
-    def receive = {
-      case SendPacket(sessionId: String, packet: Packet) => {
-        val session = sessions.get(sessionId).get //TODO: Should be getOrElse, sending non existing socket IO data to dead letter queue
-        session.channel.push(Parser.encodePacket(packet))
-        session.schedule.cancel()
-        session.schedule = Akka.system.scheduler.scheduleOnce(timeout) {
-          heartbeatHandler ! Heartbeat(sessionId)
-        }
+  def beforeConnected: Receive = {
+    case x =>
+      sender ! "1::"
+      context.become(afterConnected orElse enqueue)
+  }
 
-      }
-    }
-  }).withRouter(FromConfig()), "packetSender")
+  def afterConnected: Receive = {
 
-  def processMessage: PartialFunction[(String, (String, String, Any)), Unit]
-
-  def receive = {
-
-    case Join(sessionId) => {
-      println(sessionId)
-      val channel = Enumerator.imperative[String]()
-      if (sessions.contains(sessionId)) {
-        sender ! NotifyConnectFailure(Json.stringify(Json.toJson(Map("error" -> "Invalid Session ID"))))
-      } else {
-        val heartbeatSchedule = Akka.system.scheduler.scheduleOnce(timeout, heartbeatHandler, Heartbeat(sessionId))
-        sessions = sessions + (sessionId -> SocketIOSession(channel, heartbeatSchedule))
-        sender ! ConnectionEstablished(channel)
-      }
+    case EventOrNoop => {
+      val s = sender
+      sendEventOrNoop(s)
     }
 
-    case ProcessRawSocketData(sessionId, socketData) => {
+    case ProcessPacket(socketData) => {
+      val s = sender
       val packet = Parser.decodePacket(socketData)
 
-      println(packet)
-
       packet.packetType match {
-        case CONNECT => {
-          connectHandler ! NotifyConnected(sessionId, packet.endpoint)
-        }
+        case DISCONNECT =>
+          s ! "0::"
+          context.stop(self)
 
-        case HEARTBEAT => {
-          /*do nothing */
+        case HEARTBEAT => sendEventOrNoop(s)
 
-          //TODO: implement disconnect on haeartbeat failure
-        }
-
-        case MESSAGE => {
-          messageHandler ! ReceiveMessage(sessionId, packet.endpoint, packet.data)
-        }
-
-        case JSON => {
-          jsonHandler ! ReceiveJsonMessage(sessionId, packet.endpoint, Json.parse(packet.data))
-        }
-
-        case EVENT => {
-          val jdata: JsValue = Json.parse(packet.data)
-          eventHandler ! ReceiveEvent(sessionId, packet.endpoint, (jdata \ "name").asOpt[String].getOrElse("UNNAMED_EVENT"), jdata \ "args")
-        }
-
-        case DISCONNECT => {
-          //self ! NotifyDisconnect(sessionId, packet.endpoint)
-        }
+        case _ =>
+          val sessionId = xhrMap.find {
+            case (_, y) => y == self
+          }.get._1
+          processMessage(sessionId, packet)
+          sendEventOrNoop(s)
 
       }
-
     }
-    
-    case SendMessage(sessionId, packet) => {
-      println("Sending connect response -- " + packet)
-      packetSender ! SendPacket(sessionId, packet)
-    }
-
-    case Quit(sessionId) => {
-      if (sessions.contains(sessionId)) {
-        val session = sessions.get(sessionId).get
-        session.schedule.cancel()
-        session.channel.close()
-        sessions = sessions - sessionId
-        println(sessionId + "--- QUIT")
-      }
-    }
-
   }
-
-  //Helper method for easy send message                                                \
-  def send(sessionId: String, msg: String) {
-    send(sessionId, "", msg)
-  }
-
-  def send(sessionId: String, namespace: String, msg: String) {
-    packetSender ! SendPacket(sessionId, Packet(packetType = MESSAGE, endpoint = namespace, data = msg))
-  }
-
-  //Helper method for easy send Json message                                                \
-  def sendJson(sessionId: String, msg: JsValue) {
-    sendJson(sessionId, "", msg)
-  }
-
-  def sendJson(sessionId: String, namespace: String, msg: JsValue) {
-    packetSender ! SendPacket(sessionId, Packet(packetType = JSON, endpoint = namespace, data = Json.stringify(msg)))
-  }
-
-  //Helper method for easy emit event                                                \
-  def emit(sessionId: String, msg: String) {
-    emit(sessionId, "", msg)
-  }
-
-  def emit(sessionId: String, namespace: String, msg: String) {
-    packetSender ! SendPacket(sessionId, Packet(packetType = EVENT, endpoint = namespace, data = msg))
-  }
-
-  def emit(sessionId: String, msg: JsValue) {
-    emit(sessionId, "", msg)
-  }
-
-  def emit(sessionId: String, namespace: String, msg: JsValue) {
-    emit(sessionId, namespace, Json.stringify(msg))
-  }
-
-  //Implement broadcast
-  def broadcast(namespace: String, msg: String) {
-    //TODO: Handle basic namespace restriction, i.e. the message should not be sent to clients/sessions that are not connected to that namespace
-    sessions.keySet.foreach({
-      send(_, namespace, msg)
-    })
-  }
-
-  
 }
 
-case class Join(sessionId: String)
+class WSActor(channel: PushEnumerator[String], processMessage: (String, Packet) => Unit, wsMap: collection.mutable.Map[String, ActorRef]) extends Actor {
 
-case class Heartbeat(sessionId: String)
+  def enqueue: Receive = {
+    case Enqueue(x) => channel.push(x)
+  }
 
-case class NotifyConnected(sessionId: String, namespace: String)
+  def sendBeat = channel.push("2::")
 
-case class ReceiveMessage(sessionId: String, namespace: String, message: String)
+  def receive = beforeConnected
 
-case class SendMessage(sessionId: String, packet: Packet)
+  def beforeConnected: Receive = {
+    case _ =>
+      channel.push("1::")
+      context.system.scheduler.schedule(10.seconds, 10.seconds)(sendBeat)
+      context.become(afterConnected orElse enqueue)
+  }
 
-case class ReceiveJsonMessage(sessionId: String, namespace: String, message: JsValue)
+  def afterConnected: Receive = {
 
-case class SendJsonMessage(sessionId: String, packet: Packet)
+    case ProcessPacket(socketData) => {
+      val packet = Parser.decodePacket(socketData)
 
-case class ReceiveEvent(sessionId: String, namespace: String, eventType: String, message: JsValue)
+      packet.packetType match {
+        case DISCONNECT =>
+          channel.close()
+          wsMap.remove {
+            wsMap.find((p: (String, ActorRef)) => p._2 == self).get._1
+          }
+          context.stop(self)
 
-case class SendEvent(sessionId: String, packet: Packet)
+        case HEARTBEAT => {}
 
-case class SendPacket(sessionId: String, packet: Packet)
+        case _ =>
+          val sessionId = wsMap.find {
+            case (_, y) => y == self
+          }.get._1
+          processMessage(sessionId, packet)
 
-//case class NotifyDisconnect(sessionId:String, namespace:String)
+      }
+    }
+  }
+}
 
-case class Quit(sessionId: String)
+case object EventOrNoop
 
-case class NotifyConnectFailure(message: String)
+case class ProcessPacket(x: String)
 
-case class ConnectionEstablished(enumerator: Enumerator[String])
-
-case class SocketIOSession(val channel: PushEnumerator[String], var schedule: Cancellable)
-
-case class ProcessRawSocketData(sessionId: String, socketData: String)
-
-
-
+case class Enqueue(x: String)
